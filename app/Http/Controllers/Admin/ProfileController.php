6b06c391173c5697ee94eb9c1621ff25d553a202
Tomoki <tomo-wnfyf@ezweb.ne.jp>
2020-03-19 14:25:03 +0000

    PHP/Laravel 16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -22,19 +22,34 @@ class ProfileController extends Controller
       $form = $request->all();
       
       unset($form['_token']);
+      
       $profile->fill($form);
       $profile->save();
       
         return redirect('admin/profile/create');
     }
     
-    public function edit()
+  
+    
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+      $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);    
+      }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
     
     public function update()
     {
-        return redirect('admin/profile/edit');
+      $profile = Profile::find($request->id);
+
+      $profile_form = $request->all();
+      
+      unset($profile_form['_token']);
+
+      $profile->fill($profile_form)->save();
+       
+      return redirect('admin/profile/edit');
     }
 }
